5524337c83be5f5cbd69268651ff8dabcd94b70f
Yusuf2204 <muhammadyusuf667788@gmail.com>
2026-02-25 20:45:57 +0700

    [ADD] - Company

--- backend/database/seeders/MenuSeeder.php
+++ backend/database/seeders/MenuSeeder.php
@@ -30,12 +30,12 @@ class MenuSeeder extends Seeder
         ]);
 
         Menus::insert([
-            ['menu_name'=>'Company','menu_path'=>'/setup/company','menu_parent_id'=>$setup->id,'menu_order'=>0],
-            ['menu_name'=>'Users','menu_path'=>'/setup/users','menu_parent_id'=>$setup->id,'menu_order'=>1],
-            ['menu_name'=>'Roles','menu_path'=>'/setup/roles','menu_parent_id'=>$setup->id,'menu_order'=>2],
-            ['menu_name'=>'Menus','menu_path'=>'/setup/menus','menu_parent_id'=>$setup->id,'menu_order'=>3],
-            ['menu_name'=>'Role Permissions','menu_path'=>'/setup/role-permissions','menu_parent_id'=>$setup->id,'menu_order'=>4],
-            ['menu_name'=>'Change Password','menu_path'=>'/setup/change-password','menu_parent_id'=>$setup->id,'menu_order'=>5],
+            ['menu_name'=>'Company','menu_path'=>'/setup/company','menu_parent_id'=>$setup->id,'menu_order'=>1],
+            ['menu_name'=>'Users','menu_path'=>'/setup/users','menu_parent_id'=>$setup->id,'menu_order'=>2],
+            ['menu_name'=>'Roles','menu_path'=>'/setup/roles','menu_parent_id'=>$setup->id,'menu_order'=>3],
+            ['menu_name'=>'Menus','menu_path'=>'/setup/menus','menu_parent_id'=>$setup->id,'menu_order'=>4],
+            ['menu_name'=>'Role Permissions','menu_path'=>'/setup/role-permissions','menu_parent_id'=>$setup->id,'menu_order'=>5],
+            ['menu_name'=>'Change Password','menu_path'=>'/setup/change-password','menu_parent_id'=>$setup->id,'menu_order'=>6],
         ]);
     }
 }
